fb6e331d4c95a3d165d1175b618443def385f0d4
RafaelMacena <rafaelsouzasouza1945@gmail.com>
2022-03-09 20:02:31 -0300

    First commit: project structure created

---
+++ src/main/java/br/com/rafaelmacena/Bookstoremanager/BookstoremanagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafaelmacena.Bookstoremanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoremanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoremanagerApplication.class, args);
+	}
+
+}
